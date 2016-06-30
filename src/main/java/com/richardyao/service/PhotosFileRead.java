b0d51e9b67f66c9598127e26ec4edd86b8f4d7ac
RichardYao <richardyaoyxs@hotmail.com>
2016-06-30 20:56:24 +0800

    add new java class and change jre from 1.5 to 1.6

---
+++ src/main/java/com/richardyao/service/PhotosFileRead.java
@@ -0,0 +1,11 @@
+package com.richardyao.service;
+
+public class PhotosFileRead {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("show photos file read");
+
+	}
+
+}
